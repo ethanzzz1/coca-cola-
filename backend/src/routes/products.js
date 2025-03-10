a023cc1d91fde23774153890b3190d663bb5f52f
ethan zzz <20230006@ricaldone.edu.sv>
2025-03-10 09:35:03 -0600

    roro

--- backend/src/routes/products.js
+++ backend/src/routes/products.js
@@ -1,13 +1,14 @@
 //en este archivo dentro de la carpeta routes vamos a colocar que metodos tiene la ruta "api/products"
 
 import express from "express";
+import productsController from "../controllers/productsController.js";
 
 const router = express.Router();
 
 router.route("/")
-.get()
-.post()
-.put()
-.delete();
+.get(productsController.getProducts)
+.post(productsController.insertProducts)
+.put(productsController.updateProducts)
+.delete(productsController.deleteProduct);
 
 export default router;
